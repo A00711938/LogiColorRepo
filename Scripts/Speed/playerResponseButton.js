41b39ff2f621dce7b4789bd1bd347dd8ed7f37ab
Kuanysh Boranbayev <17136@mail.ru>
2016-05-25 13:43:48 -0700

    Shapes are implemented into speedmode

--- Scripts/Speed/playerResponseButton.js
+++ Scripts/Speed/playerResponseButton.js
@@ -4,10 +4,18 @@
  *                                      EQUATION.JS               *
  ******************************************************************/
 function getAnswer(){
-	return colorCheck() && numberCheck();
+	return colorCheck() && numberCheck(); // For numbers
+}
+function getAnswer2() { // For shapes
+	return colorCheck();
 }
 function userAnswer(myAns){
-	var questionAns = getAnswer();
+	var questionAns;
+	if (checkAnswer == 0) {
+		questionAns = getAnswer();
+	} else if (checkAnswer == 1) {
+		questionAns = getAnswer2();
+	}
 	var message = document.getElementById('message');
 	
 	if(myAns == questionAns){
